e1ed85090c78ac82b1a3a90fc43318d990bc94ad
Circle-Squar <shenyuangongzuo@163.com>
2021-04-01 21:13:58 +0800

    Add touchpad N Mouse switcher scripts; Wacom scripts now can loop

--- start-up-scripts/Wacom.sh
+++ start-up-scripts/Wacom.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
-wNum=$(xinput|grep -i wacom|grep -i stylus|cut -d = -f 2|cut -d '[' -f 1)
-xsetwacom set $wNum rotate half
-xsetwacom set $wNum area 0 0 21600 12150 
+while true
+do
+	sleep 3s
+	wNum=$(xinput|grep -i wacom|grep -i stylus|cut -d = -f 2|cut -d '[' -f 1)
+	xsetwacom set $wNum rotate half
+	xsetwacom set $wNum area 0 0 21600 12150 
+done
